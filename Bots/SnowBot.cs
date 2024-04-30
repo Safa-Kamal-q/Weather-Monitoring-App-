7f6c43f8d819d9b5b7a3e6b9211a58435a0d71bf
unknown <Safa.Qasrawi.K@gmail.com>
2024-05-01 01:16:32 +0300

    add bot file and BotConfig model

---
+++ Bots/SnowBot.cs
@@ -0,0 +1,24 @@
+﻿using Weather_Monitoring_System.Models;
+
+namespace Weather_Monitoring_System.Bots
+{
+    public class SnowBot : Bot
+    {
+        private readonly BotConfig _config;
+
+        public SnowBot()
+        {
+            _config = LoadConfig("SnowBot");
+
+        }
+
+        public override void Active(WeatherData weatherData)
+        {
+            if (_config.Enabled && weatherData.Temperature < _config.Threshold)
+            {
+                Console.WriteLine($"{GetType().Name} activated!");
+                Console.WriteLine($"{GetType().Name}: {_config.Message}");
+            }
+        }
+    }
+}
