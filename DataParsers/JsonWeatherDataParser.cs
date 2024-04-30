ee17ed6b24ffb5c387898929a8e0d4e750330fa8
unknown <Safa.Qasrawi.K@gmail.com>
2024-04-30 20:50:30 +0300

    add DataParsers files

---
+++ DataParsers/JsonWeatherDataParser.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Weather_Monitoring_System.Models;
+
+namespace Weather_Monitoring_System.DataParsers
+{
+    public class JsonWeatherDataParser: IWeatherDataParser
+    {
+        public WeatherData? Parse(string data)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<WeatherData>(data);
+            }
+            catch (JsonException ex)
+            {
+                throw new JsonException($"Error parsing JSON data: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while parsing JSON data: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ DataParsers/XmlWeatherDataParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+using Weather_Monitoring_System.Models;
+
+namespace Weather_Monitoring_System.DataParsers
+{
+    internal class XmlWeatherDataParser: IWeatherDataParser
+    {
+        public WeatherData Parse(string data)
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(WeatherData));
+
+                XmlReader reader = XmlReader.Create(new StringReader(data));
+
+                return (WeatherData)serializer.Deserialize(reader);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new InvalidOperationException($"Error parsing XML data. Invalid format or structure: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while parsing XML data: {ex.Message}");
+            }
+        }
+    }
+}
