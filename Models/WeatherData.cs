d4d0da23eb320df6c249bf9b5fdc11efc5b4e3b9
unknown <Safa.Qasrawi.K@gmail.com>
2024-04-30 18:24:37 +0300

    Add weatherData model

---
+++ Models/WeatherData.cs
@@ -0,0 +1,11 @@
+﻿namespace Weather_Monitoring_System.Models
+{
+    public class WeatherData
+    {
+        public string Location { get; set; }
+
+        public decimal Temperature { get; set; }
+
+        public decimal Humidity { get; set; }
+    }
+}
