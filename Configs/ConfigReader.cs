2a75997891aeddc82f05c3ea8b31930c3468d703
unknown <Safa.Qasrawi.K@gmail.com>
2024-05-05 13:39:27 +0300

    delete try catch in ReadConfig()

--- Configs/ConfigReader.cs
+++ Configs/ConfigReader.cs
@@ -9,21 +9,9 @@ namespace Weather_Monitoring_System.Configs
 
         public static JObject ReadConfig()
         {
-            try
-            {
-                using StreamReader file = File.OpenText(ConfigFilePath);
-                using JsonTextReader reader = new(file);
-                return JObject.Load(reader);
-            }
-            catch (FileNotFoundException)
-            {
-                throw new FileNotFoundException($"Error: Configuration file '{ConfigFilePath}' not found.");
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Error reading configuration file: {ex.Message}");
-            }
-
+            using StreamReader file = File.OpenText(ConfigFilePath);
+            using JsonTextReader reader = new(file);
+            return JObject.Load(reader);
         }
     }
 }
