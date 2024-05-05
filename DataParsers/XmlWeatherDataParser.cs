4b9e5215d84f18333d02e4802076a31671e5a3c4
unknown <Safa.Qasrawi.K@gmail.com>
2024-05-05 13:42:23 +0300

    delete try catch from parse method

--- DataParsers/XmlWeatherDataParser.cs
+++ DataParsers/XmlWeatherDataParser.cs
@@ -4,26 +4,15 @@ using Weather_Monitoring_System.Models;
 
 namespace Weather_Monitoring_System.DataParsers
 {
-    internal class XmlWeatherDataParser: IWeatherDataParser
+    internal class XmlWeatherDataParser : IWeatherDataParser
     {
         public WeatherData Parse(string data)
         {
-            try
-            {
-                var serializer = new XmlSerializer(typeof(WeatherData));
+            var serializer = new XmlSerializer(typeof(WeatherData));
 
-                XmlReader reader = XmlReader.Create(new StringReader(data));
+            XmlReader reader = XmlReader.Create(new StringReader(data));
 
-                return (WeatherData)serializer.Deserialize(reader);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw new InvalidOperationException($"Error parsing XML data. Invalid format or structure: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"An error occurred while parsing XML data: {ex.Message}");
-            }
+            return (WeatherData)serializer.Deserialize(reader);
         }
     }
 }
